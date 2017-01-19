2883433b5969300b686e5c52f0e5096052774516
Kevin <1985088337@qq.com>
2017-01-19 16:53:40 +0800

    完成消息统计接口

---
+++ WX/OS.Social.WX/Offcial/Statistic/WxOffMsgStatApi.cs
@@ -0,0 +1,142 @@
+﻿
+
+#region Copyright (C) 2017 Kevin (OS系列开源项目)
+
+/***************************************************************************
+*　　	文件功能描述：公号的功能接口 ——  消息统计接口
+*
+*　　	创建人： Kevin
+*       创建人Email：1985088337@qq.com
+*    	创建日期：2017-1-19
+*       
+*****************************************************************************/
+
+#endregion
+
+using System;
+using OS.Http;
+using OS.Http.Models;
+using OS.Social.WX.Offcial.Statistic.Mos;
+
+namespace OS.Social.WX.Offcial.Statistic
+{
+   public partial class WxOffcialStatisticApi
+   {
+       /// <summary>
+       /// 获取消息统计概览
+       ///  时间宽度最多 【七天】
+       /// </summary>
+       /// <param name="statisticReq">时间跨度最多【七天】</param>
+       /// <returns></returns>
+       public WxMsgUpStatResp<WxMsgUpStatMo> GetUpMsgStatistic(WxStatisticReq statisticReq)
+       {
+            var req=new OsHttpRequest();
+            req.HttpMothed=HttpMothed.POST;
+           req.AddressUrl = string.Concat(m_ApiUrl, "/datacube/getupstreammsg");
+           req.CustomBody = GetRequestBody(statisticReq);
+
+           return RestCommonOffcial<WxMsgUpStatResp<WxMsgUpStatMo>>(req);
+       }
+
+
+        /// <summary>
+        /// 获取【分时】消息统计概览
+        /// </summary>
+        /// <param name="date"></param>
+        /// <returns></returns>
+        public WxMsgUpStatResp<WxMsgUpStatHourMo> GetUpMsgHourStatistic(DateTime date)
+        {
+            var req = new OsHttpRequest();
+            req.HttpMothed = HttpMothed.POST;
+            req.AddressUrl = string.Concat(m_ApiUrl, "/datacube/getupstreammsghour");
+            req.CustomBody = GetRequestBody(new WxStatisticReq() {begin_date = date, end_date = date});
+
+            return RestCommonOffcial<WxMsgUpStatResp<WxMsgUpStatHourMo>>(req);
+        }
+
+
+        /// <summary>
+        /// 获取【周】消息统计概览
+        ///  时间宽度最多 【三十天】
+        /// </summary>
+        /// <param name="statisticReq">时间跨度最多【三十天】</param>
+        /// <returns></returns>
+        public WxMsgUpStatResp<WxMsgUpStatMo> GetUpMsgWeekStatistic(WxStatisticReq statisticReq)
+        {
+            var req = new OsHttpRequest();
+            req.HttpMothed = HttpMothed.POST;
+            req.AddressUrl = string.Concat(m_ApiUrl, "/datacube/getupstreammsgweek");
+            req.CustomBody = GetRequestBody(statisticReq);
+
+            return RestCommonOffcial<WxMsgUpStatResp<WxMsgUpStatMo>>(req);
+        }
+
+
+        /// <summary>
+        /// 获取【月】消息统计概览
+        ///  时间宽度最多 【三十天】
+        /// </summary>
+        /// <param name="statisticReq">时间跨度最多【三十天】</param>
+        /// <returns></returns>
+        public WxMsgUpStatResp<WxMsgUpStatMo> GetUpMsgMonthStatistic(WxStatisticReq statisticReq)
+        {
+            var req = new OsHttpRequest();
+            req.HttpMothed = HttpMothed.POST;
+            req.AddressUrl = string.Concat(m_ApiUrl, "/datacube/getupstreammsgmonth");
+            req.CustomBody = GetRequestBody(statisticReq);
+
+            return RestCommonOffcial<WxMsgUpStatResp<WxMsgUpStatMo>>(req);
+        }
+
+
+
+        /// <summary>
+        /// 获取消息【分布】统计概览
+        ///  时间宽度最多 【十五天】
+        /// </summary>
+        /// <param name="statisticReq">时间跨度最多【十五天】</param>
+        /// <returns></returns>
+        public WxMsgUpStatResp<WxMsgUpStatDistMo> GetUpMsgDistStatistic(WxStatisticReq statisticReq)
+        {
+            var req = new OsHttpRequest();
+            req.HttpMothed = HttpMothed.POST;
+            req.AddressUrl = string.Concat(m_ApiUrl, "/datacube/getupstreammsgdist");
+            req.CustomBody = GetRequestBody(statisticReq);
+
+            return RestCommonOffcial<WxMsgUpStatResp<WxMsgUpStatDistMo>>(req);
+        }
+
+        /// <summary>
+        /// 获取【周】消息【分布】统计概览
+        ///  时间宽度最多 【三十天】
+        /// </summary>
+        /// <param name="statisticReq">时间跨度最多【三十天】</param>
+        /// <returns></returns>
+        public WxMsgUpStatResp<WxMsgUpStatDistMo> GetUpMsgDistWeekStatistic(WxStatisticReq statisticReq)
+        {
+            var req = new OsHttpRequest();
+            req.HttpMothed = HttpMothed.POST;
+            req.AddressUrl = string.Concat(m_ApiUrl, "/datacube/getupstreammsgdistweek");
+            req.CustomBody = GetRequestBody(statisticReq);
+
+            return RestCommonOffcial<WxMsgUpStatResp<WxMsgUpStatDistMo>>(req);
+        }
+
+
+        /// <summary>
+        /// 获取【月】消息【分布】统计概览
+        ///  时间宽度最多 【三十天】
+        /// </summary>
+        /// <param name="statisticReq">时间跨度最多【三十天】</param>
+        /// <returns></returns>
+        public WxMsgUpStatResp<WxMsgUpStatDistMo> GetUpMsgDistMonthStatistic(WxStatisticReq statisticReq)
+        {
+            var req = new OsHttpRequest();
+            req.HttpMothed = HttpMothed.POST;
+            req.AddressUrl = string.Concat(m_ApiUrl, "/datacube/getupstreammsgdistmonth");
+            req.CustomBody = GetRequestBody(statisticReq);
+
+            return RestCommonOffcial<WxMsgUpStatResp<WxMsgUpStatDistMo>>(req);
+        }
+    }
+}
