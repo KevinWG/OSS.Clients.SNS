951f3da076ab1d74ce4c067a3adaf0cd3d0a0c31
Kevin <1985088337@qq.com>
2017-02-16 21:54:46 +0800

    添加.net core 测试站点文件

---
+++ Samples/OSS.Social.Samples/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OSS.Http.Mos;
+using OSS.Social.WX;
+using OSS.Social.WX.Offcial.Basic;
+
+namespace OSS.Social.Samples.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static WxAppCoinfig m_Config = new WxAppCoinfig()
+        {
+            AppSource = "11",
+            AppId = "wxaa9e6cb3f03afa97",
+            AppSecret = "0fc0c6f735a90fda1df5fc840e010144"
+        };
+        
+        private static WxOffBasicApi api = new WxOffBasicApi(m_Config);
+        public IActionResult Index()
+        {
+            //var token= api.GetAccessTokenAsync().WaitResult();
+            //return Content($"accesstoken:{token.access_token}");
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
