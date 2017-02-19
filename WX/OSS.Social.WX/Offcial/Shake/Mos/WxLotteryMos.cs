101d877cfdaec046abf88972027aba833289abc0
Kevin <1985088337@qq.com>
2017-02-19 19:46:56 +0800

    完成摇一摇红包（除预下单）接口

---
+++ WX/OSS.Social.WX/Offcial/Shake/Mos/WxLotteryMos.cs
@@ -0,0 +1,263 @@
+﻿#region Copyright (C) 2017 Kevin (OS系列开源项目)
+
+/***************************************************************************
+*　　	文件功能描述：公号的功能接口 —— 摇一摇红包实体
+*
+*　　	创建人： Kevin
+*       创建人Email：1985088337@qq.com
+*    	创建日期：2017-2-18
+*       
+*****************************************************************************/
+
+#endregion
+
+using System.Collections.Generic;
+
+namespace OSS.Social.WX.Offcial.Shake.Mos
+{
+    #region  创建红包活动
+    /// <summary>
+    ///   创建红包活动请求实体
+    /// </summary>
+    public class WxAddLotteryReq
+    {
+        /// <summary>   
+        ///    string 抽奖活动名称（选择使用模板时，也作为摇一摇消息主标题），最长6个汉字，12个英文字母。
+        /// </summary>  
+        public string title { get; set; }
+
+        /// <summary>   
+        ///    string 抽奖活动描述（选择使用模板时，也作为摇一摇消息副标题），最长7个汉字，14个英文字母。
+        /// </summary>  
+        public string desc { get; set; }
+
+        /// <summary>   
+        ///    int 抽奖开关。0关闭，1开启，默认为1
+        /// </summary>  
+        public int onoff { get; set; }
+
+        /// <summary>   
+        ///    long 抽奖活动开始时间，unix时间戳，单位秒
+        /// </summary>  
+        public long begin_time { get; set; }
+
+        /// <summary>   
+        ///    long 抽奖活动结束时间，unix时间戳，单位秒,红包活动有效期最长为91天
+        /// </summary>  
+        public long expire_time { get; set; }
+
+        /// <summary>   
+        ///    string 红包提供商户公众号的appid，需与预下单中的公众账号appid（wxappid）一致
+        /// </summary>  
+        public string sponsor_appid { get; set; }
+
+        /// <summary>   
+        ///    long 红包总数，红包总数是录入红包ticket总数的上限，因此红包总数应该大于等于预下单时红包ticket总数。
+        /// </summary>  
+        public long total { get; set; }
+
+        /// <summary>   
+        ///    string 红包关注界面后可以跳转到第三方自定义的页面
+        /// </summary>  
+        public string jump_url { get; set; }
+
+        /// <summary>   
+        ///    string 开发者自定义的key，用来生成活动抽奖接口的签名参数，长度32位。使用方式见sign生成规则
+        /// </summary>  
+        public string key { get; set; }
+
+
+    }
+    /// <summary>
+    /// 创建红包活动响应实体
+    /// </summary>
+    public class WxAddLotteryResp : WxBaseResp
+    {
+        /// <summary>   
+        ///    string 生成的红包活动id
+        /// </summary>  
+        public string lottery_id { get; set; }
+
+        /// <summary>   
+        ///    int 生成的模板页面ID
+        /// </summary>  
+        public int page_id { get; set; }
+
+    }
+
+    #endregion
+
+    #region  设置红包活动对应的红包信息
+    /// <summary>
+    /// 设置红包活动对应的红包信息请求
+    /// </summary>
+    public class WxSetLotteryPrizeReq
+    {
+        /// <summary>   
+        ///    string 红包抽奖id，来自addlotteryinfo返回的lottery_id
+        /// </summary>  
+        public string lottery_id { get; set; }
+
+        /// <summary>   
+        ///    string 红包提供者的商户号，，需与预下单中的商户号mch_id一致
+        /// </summary>  
+        public string mchid { get; set; }
+
+        /// <summary>   
+        ///    string 红包提供商户公众号的appid，需与预下单中的公众账号appid（wxappid）一致
+        /// </summary>  
+        public string sponsor_appid { get; set; }
+
+        /// <summary>   
+        ///    json数组 红包ticket列表，如果红包数较多，可以一次传入多个红包，批量调用该接口设置红包信息。每次请求传入的红包个数上限为100
+        /// </summary>  
+        public List<WxLotteryTicketMo> prize_info_list { get; set; }
+    }
+
+    public class WxLotteryTicketMo
+    {
+        /// <summary>   
+        ///    string 预下单时返回的红包ticket，单个活动红包ticket数量上限为100000个，可添加多次。
+        /// </summary>  
+        public string ticket { get; set; }
+    }
+
+    /// <summary>
+    /// 设置红包活动对应的红包信息响应实体
+    /// </summary>
+    public class WxSetLotteryPrizeResp:WxBaseResp
+    {
+        /// <summary>   
+        ///    array 重复使用的ticket列表，如为空，将不返回
+        /// </summary>  
+        public List<WxLotteryTicketMo> repeat_ticket_list { get; set; }
+
+        /// <summary>   
+        ///    array 过期的ticket列表，如为空，将不返回
+        /// </summary>  
+        public List<WxLotteryTicketMo> expire_ticket_list { get; set; }
+
+        /// <summary>   
+        ///    array 金额不在大于1元，小于1000元的ticket列表，如为空，将不返回
+        /// </summary>  
+        public List<WxLotteryTicketMo> invalid_amount_ticket_list { get; set; }
+
+        /// <summary>   
+        ///    int 成功录入的红包数量
+        /// </summary>  
+        public int success_num { get; set; }
+
+        /// <summary>   
+        ///    array 原因：生成红包的时候，授权商户号auth_mchid和auth_appid没有写摇周边的商户号
+        /// </summary>  
+        public List<WxLotteryTicketMo> wrong_authmchid_ticket_list { get; set; }
+
+        /// <summary>   
+        ///    array ticket解析失败，可能有错别字符或不完整
+        /// </summary>  
+        public List<WxLotteryTicketMo> invalid_ticket_list { get; set; }
+
+
+    }
+
+    #endregion
+
+
+    #region 红包查询接口
+    /// <summary>
+    ///  查询红包响应实体
+    /// </summary>
+    public class WxGetLotteryResp:WxBaseResp
+    {
+        /// <summary>
+        ///  结果实体
+        /// </summary>
+        public WxLotteryInfoMo result { get; set; }
+    }
+
+    public class WxLotteryInfoMo
+    {
+        /// <summary>   
+        ///    string 抽奖活动名称（选择使用模板时，也作为摇一摇消息主标题），最长6个汉字，12个英文字母。
+        /// </summary>  
+        public string title { get; set; }
+
+        /// <summary>   
+        ///    string 抽奖活动描述（选择使用模板时，也作为摇一摇消息副标题），最长7个汉字，14个英文字母。
+        /// </summary>  
+        public string desc { get; set; }
+
+        /// <summary>   
+        ///    int 抽奖开关。0关闭，1开启，默认为1
+        /// </summary>  
+        public int onoff { get; set; }
+
+        /// <summary>   
+        ///    long 抽奖活动开始时间，unix时间戳，单位秒
+        /// </summary>  
+        public long begin_time { get; set; }
+
+        /// <summary>   
+        ///    long 抽奖活动结束时间，unix时间戳，单位秒，红包活动有效期最长为91天
+        /// </summary>  
+        public long expire_time { get; set; }
+
+        /// <summary>   
+        ///    string 红包提供商户公众号的appid
+        /// </summary>  
+        public string sponsor_appid { get; set; }
+
+        /// <summary>   
+        ///    string 创建活动的开发者appid
+        /// </summary>  
+        public string appid { get; set; }
+
+        /// <summary>   
+        ///    long 已录入的红包总数
+        /// </summary>  
+        public long prize_count { get; set; }
+
+        /// <summary>   
+        ///    long 创建活动时预设的录入红包ticket数量上限
+        /// </summary>  
+        public long prize_count_limit { get; set; }
+
+        /// <summary>   
+        ///    string 红包关注界面后可以跳转到第三方自定义的页面
+        /// </summary>  
+        public string jump_url { get; set; }
+
+        /// <summary>   
+        ///    long 过期红包ticket数量
+        /// </summary>  
+        public long expired_prizes { get; set; }
+
+        /// <summary>   
+        ///    long 已发放的红包ticket数量
+        /// </summary>  
+        public long drawed_prizes { get; set; }
+
+        /// <summary>   
+        ///    long 可用的红包ticket数量
+        /// </summary>  
+        public long available_prizes { get; set; }
+
+        /// <summary>   
+        ///    long 已过期的红包金额总和
+        /// </summary>  
+        public long expired_value { get; set; }
+
+        /// <summary>   
+        ///    long 已发放的红包金额总和
+        /// </summary>  
+        public long drawed_value { get; set; }
+
+        /// <summary>   
+        ///    long 可用的红包金额总和
+        /// </summary>  
+        public long available_value { get; set; }
+    }
+
+    #endregion
+
+}
---
+++ WX/OSS.Social.WX/Offcial/Shake/WxOffLotteryApi.cs
@@ -0,0 +1,105 @@
+﻿#region Copyright (C) 2017 Kevin (OS系列开源项目)
+
+/***************************************************************************
+*　　	文件功能描述：公号的功能接口 —— 摇一摇红包接口
+*
+*　　	创建人： Kevin
+*       创建人Email：1985088337@qq.com
+*    	创建日期：2017-2-18
+*       
+*****************************************************************************/
+
+#endregion
+
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using OSS.Http.Mos;
+using OSS.Social.WX.Offcial.Shake.Mos;
+
+namespace OSS.Social.WX.Offcial.Shake
+{
+    /// <summary>
+    /// 微信的摇一摇接口
+    /// </summary>
+    public partial class WxOffShakeApi : WxOffBaseApi
+    {
+        public WxOffShakeApi(WxAppCoinfig config) : base(config)
+        {
+        }
+    }
+
+
+    /// <summary>
+    ///   微信摇一摇红包接口
+    /// </summary>
+    public partial class WxOffShakeApi
+    {
+        /// <summary>
+        /// 创建红包活动
+        /// </summary>
+        /// <param name="userTemplate">是否使用模板，1：使用，2：不使用,以参数的形式拼装在url后。（模版即交互流程图中的红包加载页，使用模板用户不需要点击可自动打开红包；不使用模版需自行开发HTML5页面，并在页面调用红包jsapi）</param>
+        /// <param name="logUrl">使用模板页面的logo_url，不使用模板时可不加。展示在摇一摇界面的消息图标。图片尺寸为120x120</param>
+        /// <param name="lotteryReq">活动对应内容</param>
+        /// <returns></returns>
+        public async Task<WxAddLotteryResp> AddLottery(int userTemplate, string logUrl, WxAddLotteryReq lotteryReq)
+        {
+            var req = new OsHttpRequest();
+
+            req.HttpMothed = HttpMothed.POST;
+            req.AddressUrl = string.Concat(m_ApiUrl,
+                $"/shakearound/lottery/addlotteryinfo?use_template={userTemplate}&logo_url={logUrl}");
+            req.CustomBody = JsonConvert.SerializeObject(lotteryReq);
+
+            return await RestCommonOffcialAsync<WxAddLotteryResp>(req);
+        }
+
+        /// <summary>
+        ///   录入红包信息
+        /// </summary>
+        /// <param name="prizeReq">红包内容</param>
+        /// <returns></returns>
+        public async Task<WxSetLotteryPrizeResp> SetLotteryPrize(WxSetLotteryPrizeReq prizeReq)
+        {
+            var req = new OsHttpRequest();
+
+            req.HttpMothed = HttpMothed.POST;
+            req.AddressUrl = string.Concat(m_ApiUrl,"/shakearound/lottery/setprizebucket");
+            req.CustomBody = JsonConvert.SerializeObject(prizeReq);
+
+            return await RestCommonOffcialAsync<WxSetLotteryPrizeResp>(req);
+        }
+
+        /// <summary>
+        /// 设置红包活动抽奖开关
+        /// </summary>
+        /// <param name="lotteryId">红包抽奖id，来自addlotteryinfo返回的lottery_id</param>
+        /// <param name="onoff">活动抽奖开关，0：关闭，1：开启</param>
+        /// <returns></returns>
+        public async Task<WxBaseResp> SetLotterySwitch(string lotteryId, int onoff)
+        {
+            var req = new OsHttpRequest();
+
+            req.HttpMothed = HttpMothed.GET;
+            req.AddressUrl = string.Concat(m_ApiUrl, $"/shakearound/lottery/setlotteryswitch?lottery_id={lotteryId}&onoff={onoff}");
+
+            return await RestCommonOffcialAsync<WxBaseResp>(req);
+        }
+
+        /// <summary>
+        /// 查询红包信息
+        /// </summary>
+        /// <param name="lotteryId">红包抽奖id，来自addlotteryinfo返回的lottery_id</param>
+        /// <returns></returns>
+        public async Task<WxGetLotteryResp> GetLotterySwitch(string lotteryId)
+        {
+            var req = new OsHttpRequest();
+
+            req.HttpMothed = HttpMothed.GET;
+            req.AddressUrl = string.Concat(m_ApiUrl, "/shakearound/lottery/querylottery?lottery_id=",lotteryId);
+
+            return await RestCommonOffcialAsync<WxGetLotteryResp>(req);
+        }
+    }
+
+
+}
