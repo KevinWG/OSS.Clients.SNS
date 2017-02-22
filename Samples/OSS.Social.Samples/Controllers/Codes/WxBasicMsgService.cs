b1c9f60e74da119357ab871695c00b3b0c7e5226
Kevin <1985088337@qq.com>
2017-02-22 23:36:08 +0800

    添加直接返回空的示例

--- Samples/OSS.Social.Samples/Controllers/Codes/WxBasicMsgService.cs
+++ Samples/OSS.Social.Samples/Controllers/Codes/WxBasicMsgService.cs
@@ -13,7 +13,11 @@ namespace OSS.Social.Samples.Controllers.Codes
 
         private BaseReplyMsg WxBasicMsgService_TextHandler(TextRecMsg arg)
         {
-            return new TextReplyMsg(){ Content = "您好，如果您需要地陪服务请关注公众号“口袋地陪”！"};
+            if (arg.Content=="oss")
+            {
+                return new TextReplyMsg() { Content = "欢迎关注.Net开源世界！" };
+            }
+            return null;
         }
 
         protected override void ProcessEnd(MsgContext msgContext)
