81eaadd2e7206791c08a0644ee4d7fd0cdabbfc4
Kevin <1985088337@qq.com>
2017-09-04 00:05:28 +0800

    添加平台授权相关类

---
+++ Sns/Official/OSS.SnsSdk.Official.Wx/Platform/Mos/WxPlatformTokenMos.cs
@@ -0,0 +1,22 @@
+﻿#region MyRegion
+
+/***************************************************************************
+*　　	文件功能描述：公号的功能接口 —— 获取平台授权token相关实体
+*
+*　　	创建人： Kevin
+*       创建人Email：1985088337@qq.com
+*    	创建日期：2017-9-3
+*       
+*****************************************************************************/
+
+#endregion
+namespace OSS.SnsSdk.Official.Wx.Platform.Mos
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class WxGetPlatformAccessTokenReq
+    {
+
+    }
+}
---
+++ Sns/Official/OSS.SnsSdk.Official.Wx/Platform/WxOffPlatformApi.cs
@@ -0,0 +1,34 @@
+﻿#region Copyright (C) 2017 Kevin (OSS开源作坊) 公众号：osscoder
+
+/***************************************************************************
+*　　	文件功能描述：公号的功能接口 —— 微信开放平台相关接口
+*
+*　　	创建人： Kevin
+*       创建人Email：1985088337@qq.com
+*    	创建日期：2017-9-3
+*       
+*****************************************************************************/
+
+#endregion
+
+using OSS.Common.ComModels;
+
+namespace OSS.SnsSdk.Official.Wx.Platform
+{
+    /// <summary>
+    ///  微信开放平台相关接口
+    /// </summary>
+    public class WxOffPlatformApi: WxOffBaseApi
+    {
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="config"></param>
+        public WxOffPlatformApi(AppConfig config=null) : base(config)
+        {
+        }
+
+        //public 
+
+    }
+}
